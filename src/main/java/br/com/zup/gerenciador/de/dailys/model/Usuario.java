1b09ce6adedfd59b43fa194eb3cf3023df49905c
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-01-24 09:08:29 -0300

    Criar entidade Usuario

---
+++ src/main/java/br/com/zup/gerenciador/de/dailys/model/Usuario.java
@@ -0,0 +1,67 @@
+package br.com.zup.gerenciador.de.dailys.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+
+    @Id
+    private String email;
+    private String senha;
+    private String nome;
+    private Atividade taskAtual;
+    private Atividade proximaTask;
+    private Atividade impedimento;
+
+    public Usuario() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Atividade getTaskAtual() {
+        return taskAtual;
+    }
+
+    public void setTaskAtual(Atividade taskAtual) {
+        this.taskAtual = taskAtual;
+    }
+
+    public Atividade getProximaTask() {
+        return proximaTask;
+    }
+
+    public void setProximaTask(Atividade proximaTask) {
+        this.proximaTask = proximaTask;
+    }
+
+    public Atividade getImpedimento() {
+        return impedimento;
+    }
+
+    public void setImpedimento(Atividade impedimento) {
+        this.impedimento = impedimento;
+    }
+}
