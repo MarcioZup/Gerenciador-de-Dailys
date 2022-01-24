17c6487074f9b9d9fc750b50b8fd21013b0ed611
Karen Almeida <karenalmeida61@hotmail.com>
2022-01-24 14:21:39 -0300

    Implementar metodo salvar atividade

--- src/main/java/br/com/zup/gerenciador/de/dailys/services/AtividadeService.java
+++ src/main/java/br/com/zup/gerenciador/de/dailys/services/AtividadeService.java
@@ -1,10 +1,18 @@
 package br.com.zup.gerenciador.de.dailys.services;
 
+import br.com.zup.gerenciador.de.dailys.model.Atividade;
 import br.com.zup.gerenciador.de.dailys.repositories.AtividadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AtividadeService {
-private AtividadeRepository atividadeRepository;
+
+    @Autowired
+    private AtividadeRepository atividadeRepository;
+
+    public Atividade salvarAtividade(Atividade atividade){
+        return atividadeRepository.save(atividade);
+    }
 
 }
