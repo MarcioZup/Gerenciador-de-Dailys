080aeecc67c3247d7f9a8817ec1c4ecc41834d41
Karen Almeida <karenalmeida61@hotmail.com>
2022-01-24 14:55:29 -0300

    Implementar metodo buscar atividade por id

--- src/main/java/br/com/zup/gerenciador/de/dailys/services/AtividadeService.java
+++ src/main/java/br/com/zup/gerenciador/de/dailys/services/AtividadeService.java
@@ -6,21 +6,36 @@ import br.com.zup.gerenciador.de.dailys.repositories.AtividadeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class AtividadeService {
 
     @Autowired
     private AtividadeRepository atividadeRepository;
 
-    public Atividade salvarAtividade(Atividade atividade){
+    public Atividade salvarAtividade(Atividade atividade) {
         return atividadeRepository.save(atividade);
     }
-    public void  deletarAtividade(Long id){
-        if(!atividadeRepository.existsById(id)){
+
+    public void deletarAtividade(Long id) {
+        if (!atividadeRepository.existsById(id)) {
             throw new AtividadeNaoEncontrada("Atividade não encontrada");
         }
         atividadeRepository.deleteById(id);
-
     }
+
+   public Atividade exibirAtividade(){
+        Atividade atividade = (Atividade) atividadeRepository.findAll();
+        return atividade;
+   }
+
+   public Atividade buscarAtividadePorId(Long id){
+        Optional<Atividade>atividadeOptional = atividadeRepository.findById(id);
+        if(atividadeOptional.isEmpty()){
+            throw new AtividadeNaoEncontrada("Atividade não encontrada");
+        }
+        return atividadeOptional.get();
+   }
 
 }
