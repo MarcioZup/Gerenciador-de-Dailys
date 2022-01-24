b9891223bf9d92312d78a00811d84617deadf6e8
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-01-24 08:40:46 -0300

    Primeiro commit. Criar projeto

---
+++ src/main/java/br/com/zup/gerenciador/de/dailys/GerenciadorDeDailysApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.gerenciador.de.dailys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeDailysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeDailysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/gerenciador/de/dailys/GerenciadorDeDailysApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.gerenciador.de.dailys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeDailysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
