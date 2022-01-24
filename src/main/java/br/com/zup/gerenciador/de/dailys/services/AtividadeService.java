9e07fa0d0de88f4aba67be170c7770b0d79ded1b
Karen Almeida <karenalmeida61@hotmail.com>
2022-01-24 14:18:25 -0300

    Implementar classe Service

--- src/main/java/br/com/zup/gerenciador/de/dailys/services/AtividadeService.java
+++ src/main/java/br/com/zup/gerenciador/de/dailys/services/AtividadeService.java
@@ -1,7 +1,10 @@
 package br.com.zup.gerenciador.de.dailys.services;
 
 import br.com.zup.gerenciador.de.dailys.repositories.AtividadeRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AtividadeService {
 private AtividadeRepository atividadeRepository;
+
 }
