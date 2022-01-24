6ce809ef68c305eb79e2969a33d338a349d92ac2
Karen Almeida <karenalmeida61@hotmail.com>
2022-01-24 14:13:30 -0300

    Implementar interface repository

---
+++ src/main/java/br/com/zup/gerenciador/de/dailys/repositories/AtividadeRepository.java
@@ -0,0 +1,8 @@
+package br.com.zup.gerenciador.de.dailys.repositories;
+
+import br.com.zup.gerenciador.de.dailys.model.Atividade;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AtividadeRepository extends CrudRepository<Atividade, Long> {
+
+}
---
+++ src/main/java/br/com/zup/gerenciador/de/dailys/services/AtividadeService.java
@@ -0,0 +1,7 @@
+package br.com.zup.gerenciador.de.dailys.services;
+
+import br.com.zup.gerenciador.de.dailys.repositories.AtividadeRepository;
+
+public class AtividadeService {
+private AtividadeRepository atividadeRepository;
+}
