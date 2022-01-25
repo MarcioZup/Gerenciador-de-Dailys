d4b8a9416d48f710563a6b183f7cde89d64b1e80
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-25 19:12:46 -0300

    Criar classe de configuração de segurança

---
+++ src/main/java/br/com/zup/gerenciador/de/dailys/config/seguranca/ConfiguracaoDeSeguranca.java
@@ -0,0 +1,7 @@
+package br.com.zup.gerenciador.de.dailys.config.seguranca;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
+
+}
