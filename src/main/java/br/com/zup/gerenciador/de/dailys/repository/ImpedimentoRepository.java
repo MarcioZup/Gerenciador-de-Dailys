41a503129d3c5ae2fc7ea62e76bbca9426ac5003
Karen Almeida <karenalmeida61@hotmail.com>
2022-01-25 15:58:30 -0300

    Criar Classe ImpedimentoRepository

---
+++ src/main/java/br/com/zup/gerenciador/de/dailys/repository/ImpedimentoRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.gerenciador.de.dailys.repository;
+
+import br.com.zup.gerenciador.de.dailys.model.Impedimento;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ImpedimentoRepository extends CrudRepository<Impedimento, Long> {
+}
