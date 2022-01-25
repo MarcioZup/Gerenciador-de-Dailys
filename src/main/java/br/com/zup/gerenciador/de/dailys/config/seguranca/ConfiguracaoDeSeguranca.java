27a72209de27d60d578e3079868af47d0144eb19
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-25 19:25:37 -0300

    Implementar configuração do CORS

--- src/main/java/br/com/zup/gerenciador/de/dailys/config/seguranca/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/gerenciador/de/dailys/config/seguranca/ConfiguracaoDeSeguranca.java
@@ -1,8 +1,12 @@
 package br.com.zup.gerenciador.de.dailys.config.seguranca;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
 
@@ -18,7 +22,9 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
+
         httpSecurity.csrf().disable();
+        httpSecurity.cors().configurationSource(configuracaoDoCORS());
 
         httpSecurity.authorizeRequests()
                 .antMatchers(HttpMethod.POST, ENDPOINT_POST_PUBLICO).permitAll()
@@ -27,6 +33,16 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
         httpSecurity.authorizeRequests()
                 .antMatchers(HttpMethod.POST, ENDPOINT_GET_PUBLICO).permitAll()
                 .anyRequest().authenticated();
+
+    }
+
+    @Bean
+    CorsConfigurationSource configuracaoDoCORS(){
+
+        UrlBasedCorsConfigurationSource cors = new UrlBasedCorsConfigurationSource();
+        cors.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return cors;
+
     }
 
 }
