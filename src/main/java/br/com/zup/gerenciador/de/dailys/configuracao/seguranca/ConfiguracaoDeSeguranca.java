22ba4eb1406deaa0a3e5ad945c84f28606eece72
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-25 14:47:33 -0300

    Definir rotas abertas da aplicação

--- src/main/java/br/com/zup/gerenciador/de/dailys/configuracao/seguranca/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/gerenciador/de/dailys/configuracao/seguranca/ConfiguracaoDeSeguranca.java
@@ -1,11 +1,34 @@
 package br.com.zup.gerenciador.de.dailys.configuracao.seguranca;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class ConfiguracaoDeSeguranca {
+public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
+    private static final String[] ENDPOINT_POST_PUBLICO = {
+            "/usuario",
+            "/tasks"
+    };
 
+    private static final String[] ENDPOINT_GET_PUBLICO = {
+            "/tasks"
+    };
+
+    @Override
+    protected void configure (HttpSecurity httpSecurity) throws Exception{
+
+        httpSecurity.authorizeRequests()
+                .antMatchers(HttpMethod.POST, ENDPOINT_POST_PUBLICO).permitAll()
+                .anyRequest().authenticated();
+
+        httpSecurity.authorizeRequests()
+                .antMatchers(HttpMethod.GET, ENDPOINT_GET_PUBLICO).permitAll()
+                .anyRequest().authenticated();
+
+    }
 
 }
