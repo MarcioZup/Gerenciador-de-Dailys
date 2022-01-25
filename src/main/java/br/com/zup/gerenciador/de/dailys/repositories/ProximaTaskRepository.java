bc760cf0033b7a0dd9be117806cc738f991ca2bf
Karen Almeida <karenalmeida61@hotmail.com>
2022-01-25 15:24:20 -0300

    Implementar crud na Classe ProximaTask

--- src/main/java/br/com/zup/gerenciador/de/dailys/repositories/ProximaTaskRepository.java
+++ src/main/java/br/com/zup/gerenciador/de/dailys/repositories/ProximaTaskRepository.java
@@ -1,6 +1,8 @@
 package br.com.zup.gerenciador.de.dailys.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProximaTaskRepository extends CrudRepository<ProximaTaskRepository, Long> {
 }
--- src/main/java/br/com/zup/gerenciador/de/dailys/services/ProximaTaskService.java
+++ src/main/java/br/com/zup/gerenciador/de/dailys/services/ProximaTaskService.java
@@ -1,10 +1,24 @@
 package br.com.zup.gerenciador.de.dailys.services;
 
+import br.com.zup.gerenciador.de.dailys.model.ProximaTask;
+import br.com.zup.gerenciador.de.dailys.repositories.ProximaTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProximaTaskService {
     @Autowired
+    private ProximaTaskRepository proximaTaskRepository;
 
+    public ProximaTask salvarProximaTask(ProximaTask proximaTask) {
+        return proximaTaskRepository.save(proximaTask);
+    }
+
+    public Iterable<ProximaTaskRepository> exibirProximasTasks() {
+        return proximaTaskRepository.findAll();
+    }
+
+    public void deletarProximaTask(Long id) {
+        proximaTaskRepository.deleteById(id);
+    }
 }
