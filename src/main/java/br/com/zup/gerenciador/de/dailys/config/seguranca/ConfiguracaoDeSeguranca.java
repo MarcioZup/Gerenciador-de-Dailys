2b604f71abb6760c6618cf1dd4f8c8536a12f2d6
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-25 19:19:59 -0300

    Implementar rotas que serão públicas na aplicação

--- src/main/java/br/com/zup/gerenciador/de/dailys/config/seguranca/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/gerenciador/de/dailys/config/seguranca/ConfiguracaoDeSeguranca.java
@@ -1,7 +1,31 @@
 package br.com.zup.gerenciador.de.dailys.config.seguranca;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
+
+    private static final String[] ENDPOINT_POST_PUBLICO = {
+            "/usuario",
+            "/task"
+    };
+
+    private static final String[] ENDPOINT_GET_PUBLICO = {
+            "/usuarios",
+            "/tasks"
+    };
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeRequests()
+                .antMatchers(HttpMethod.POST, ENDPOINT_POST_PUBLICO).permitAll()
+                .anyRequest().authenticated();
+
+        httpSecurity.authorizeRequests()
+                .antMatchers(HttpMethod.POST, ENDPOINT_GET_PUBLICO).permitAll()
+                .anyRequest().authenticated();
+    }
 
 }
