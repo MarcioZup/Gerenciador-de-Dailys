751a54b1bb82e177061b18365646a7760a1a0be7
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-25 20:00:35 -0300

    Implementar UserDetail para autenticação do usuário

---
+++ src/main/java/br/com/zup/gerenciador/de/dailys/config/seguranca/UsuarioLogado.java
@@ -0,0 +1,63 @@
+package br.com.zup.gerenciador.de.dailys.config.seguranca;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioLogado implements UserDetails {
+
+    private String email;
+    private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+
+        return true;
+    }
+
+}
