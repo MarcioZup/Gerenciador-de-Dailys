a66da0c9ce4510fb44acac6b004c96b28538f7a4
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-25 17:29:01 -0300

    Deletar usuário

--- src/main/java/br/com/zup/gerenciador/de/dailys/service/UsuarioService.java
+++ src/main/java/br/com/zup/gerenciador/de/dailys/service/UsuarioService.java
@@ -27,4 +27,8 @@ public class UsuarioService {
         return usuarioRepository.findAll();
     }
 
+    public void deletarUsuario(String email){
+        usuarioRepository.deleteById(email);
+    }
+
 }
