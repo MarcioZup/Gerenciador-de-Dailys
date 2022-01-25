a9f9787f8535c433712e32f508ebc113c1f0c041
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-25 19:53:25 -0300

    Implementar na repository localização do usuario por ID

--- src/main/java/br/com/zup/gerenciador/de/dailys/usuario/UsuarioRepository.java
+++ src/main/java/br/com/zup/gerenciador/de/dailys/usuario/UsuarioRepository.java
@@ -2,5 +2,9 @@ package br.com.zup.gerenciador.de.dailys.usuario;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends CrudRepository<Usuario, String> {
+
+    Optional<Usuario> findByEmail(String email);
 }
