ab3d6eaecec131532b4c9addbbcd962728a2bba9
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-25 18:08:29 -0300

    Implementar método para salvar um usuario

--- src/main/java/br/com/zup/gerenciador/de/dailys/service/UsuarioService.java
+++ src/main/java/br/com/zup/gerenciador/de/dailys/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package br.com.zup.gerenciador.de.dailys.service;
 
+import br.com.zup.gerenciador.de.dailys.model.Usuario;
 import br.com.zup.gerenciador.de.dailys.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,9 @@ public class UsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    public Usuario salvarUsuario (Usuario usuario){
+        return usuarioRepository.save(usuario);
+
+    }
 }
