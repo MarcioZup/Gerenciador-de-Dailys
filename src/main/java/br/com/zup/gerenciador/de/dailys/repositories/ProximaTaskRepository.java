74e1addbcf2a640694e74cd648cf94dba77c60a6
Karen Almeida <karenalmeida61@hotmail.com>
2022-01-25 15:08:52 -0300

    Criar classe repository para ProximaTask

---
+++ src/main/java/br/com/zup/gerenciador/de/dailys/repositories/ProximaTaskRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.gerenciador.de.dailys.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProximaTaskRepository extends CrudRepository<ProximaTaskRepository, Long> {
+}
---
+++ src/main/java/br/com/zup/gerenciador/de/dailys/services/ProximaTaskService.java
@@ -0,0 +1,10 @@
+package br.com.zup.gerenciador.de.dailys.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProximaTaskService {
+    @Autowired
+
+}
