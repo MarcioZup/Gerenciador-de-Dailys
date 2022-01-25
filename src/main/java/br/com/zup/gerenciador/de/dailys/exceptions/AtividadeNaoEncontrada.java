be4aa3468510e4fdc61bd69d885cf5411ca4fe63
Karen Almeida <karenalmeida61@hotmail.com>
2022-01-25 13:48:39 -0300

    Ajustes do projeto/excluir Atividade

--- src/main/java/br/com/zup/gerenciador/de/dailys/exceptions/AtividadeNaoEncontrada.java
+++
@@ -1,7 +0,0 @@
-package br.com.zup.gerenciador.de.dailys.exceptions;
-
-public class AtividadeNaoEncontrada extends RuntimeException {
-    public AtividadeNaoEncontrada(String message) {
-        super(message);
-    }
-}
