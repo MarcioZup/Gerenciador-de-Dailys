c2ac366b5a7c29c8b009482d3ae941cbe9ba816f
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-25 20:48:31 -0300

    Implementar na configuração de segurança o filtro de autenticação via JWT

--- src/main/java/br/com/zup/gerenciador/de/dailys/config/seguranca/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/gerenciador/de/dailys/config/seguranca/ConfiguracaoDeSeguranca.java
@@ -1,16 +1,26 @@
 package br.com.zup.gerenciador.de.dailys.config.seguranca;
 
+import br.com.zup.gerenciador.de.dailys.config.seguranca.jwt.FiltroDeAutenticacaoJwt;
+import br.com.zup.gerenciador.de.dailys.config.seguranca.jwt.JwtComponent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtComponent jwtComponent;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     private static final String[] ENDPOINT_POST_PUBLICO = {
             "/usuario",
@@ -38,6 +48,13 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
 
         httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
+        httpSecurity.addFilter(new FiltroDeAutenticacaoJwt(jwtComponent, authenticationManager()));
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 
     @Bean
