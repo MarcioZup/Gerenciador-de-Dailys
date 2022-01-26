f9f5679a4c45c15e5836f5144b5ebb059d8713c8
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-25 21:36:45 -0300

    Criar método para pegar autorização

--- src/main/java/br/com/zup/gerenciador/de/dailys/config/seguranca/jwt/FiltroDeAutorizacaoJwt.java
+++ src/main/java/br/com/zup/gerenciador/de/dailys/config/seguranca/jwt/FiltroDeAutorizacaoJwt.java
@@ -1,6 +1,10 @@
 package br.com.zup.gerenciador.de.dailys.config.seguranca.jwt;
 
+import br.com.zup.gerenciador.de.dailys.config.seguranca.jwt.exceptions.TokenInvalidoException;
+import io.jsonwebtoken.Claims;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -14,6 +18,20 @@ public class FiltroDeAutorizacaoJwt extends BasicAuthenticationFilter {
         super(authenticationManager);
         this.jwtComponent = jwtComponent;
         this.userDetailsService = userDetailsService;
+
+    }
+
+    public UsernamePasswordAuthenticationToken pegarAutenticacao(String token){
+        if(!jwtComponent.tokenValido(token)){
+            throw new TokenInvalidoException();
+
+        }
+
+        Claims claims = jwtComponent.pegarClaims(token);
+        UserDetails usuarioLogado = userDetailsService.loadUserByUsername(claims.getSubject());
+
+        return new UsernamePasswordAuthenticationToken(usuarioLogado, null, usuarioLogado.getAuthorities());
+
     }
 
 }
