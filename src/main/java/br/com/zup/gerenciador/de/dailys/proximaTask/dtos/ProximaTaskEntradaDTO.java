8cdb0534ad062b91e58ca9a1ecb5815334f52209
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-26 15:00:48 -0300

    Criar DTO de entrada próxima task

---
+++ src/main/java/br/com/zup/gerenciador/de/dailys/proximaTask/dtos/ProximaTaskEntradaDTO.java
@@ -0,0 +1,31 @@
+package br.com.zup.gerenciador.de.dailys.proximaTask.dtos;
+
+import br.com.zup.gerenciador.de.dailys.usuario.Usuario;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProximaTaskEntradaDTO {
+
+    @NotBlank
+    private String descricao;
+    @NotNull
+    private Date dataInicio;
+    @NotNull
+    private Date previsaoFim;
+    @Valid
+    @NotBlank
+    private Usuario usuario;
+
+}
