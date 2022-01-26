1cf011e233fdaeb2400c9b0a2c4fe781e150e439
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-26 15:33:10 -0300

    Implementar rota para salvar próxima task

--- src/main/java/br/com/zup/gerenciador/de/dailys/proximaTask/ProximaTaskController.java
+++ src/main/java/br/com/zup/gerenciador/de/dailys/proximaTask/ProximaTaskController.java
@@ -1,8 +1,12 @@
 package br.com.zup.gerenciador.de.dailys.proximaTask;
 
+import br.com.zup.gerenciador.de.dailys.proximaTask.dtos.ProximaTaskEntradaDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/proximaTask")
@@ -10,5 +14,15 @@ public class ProximaTaskController {
 
     @Autowired
     private ProximaTaskService proximaTaskService;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProximaTask salvarTask(@RequestBody @Valid ProximaTaskEntradaDTO proximaTaskEntradaDTO){
+        ProximaTask proximaTask = modelMapper.map(proximaTaskEntradaDTO, ProximaTask.class);
+
+        return proximaTaskService.salvarProximaTask(proximaTask);
+    }
 
 }
