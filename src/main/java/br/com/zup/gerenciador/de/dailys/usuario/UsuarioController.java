3317e275b30ad84e0bd6fd7ef363b78f8d7dbf8f
Karen Almeida <karenalmeida61@hotmail.com>
2022-01-26 13:29:17 -0300

    Implementar  Classe UsuarioController

--- src/main/java/br/com/zup/gerenciador/de/dailys/usuario/UsuarioController.java
+++ src/main/java/br/com/zup/gerenciador/de/dailys/usuario/UsuarioController.java
@@ -2,10 +2,16 @@ package br.com.zup.gerenciador.de.dailys.usuario;
 
 
 import br.com.zup.gerenciador.de.dailys.usuario.UsuarioService;
+import br.com.zup.gerenciador.de.dailys.usuario.dtos.UsuarioDTO;
+import br.com.zup.gerenciador.de.dailys.usuario.dtos.UsuarioFiltroDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
@@ -15,6 +21,29 @@ public class UsuarioController {
     UsuarioService usuarioService;
     @Autowired
     ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UsuarioDTO cadastrarUsuario(@RequestBody @Valid UsuarioDTO usuario) {
+        Usuario novoUsuario = modelMapper.map(usuario, Usuario.class);
+        return modelMapper.map(usuarioService.salvarUsuario(novoUsuario), UsuarioDTO.class);
+    }
+
+    @GetMapping
+    public List<UsuarioFiltroDTO> exibirUsuarios() {
+        List<UsuarioFiltroDTO> listaDeUsuarios = new ArrayList<>();
+        for (Usuario usuario : usuarioService.exibirUsuario()) {
+            UsuarioFiltroDTO usuarioFiltroDTO = modelMapper.map(usuario, UsuarioFiltroDTO.class);
+            listaDeUsuarios.add(usuarioFiltroDTO);
+        }
+        return listaDeUsuarios;
+    }
+
+    @DeleteMapping("/{email}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarUsuario(@PathVariable String email) {
+        usuarioService.deletarUsuario(email);
+    }
 
 
 }
