1e83ba20445f6de3d556a9c4e34f2b822e211a56
Karen Almeida <karenalmeida61@hotmail.com>
2022-01-26 12:20:22 -0300

    Adicionar @DeleteMapping na Classe ImpedimentoController

--- src/main/java/br/com/zup/gerenciador/de/dailys/impedimento/ImpedimentoController.java
+++ src/main/java/br/com/zup/gerenciador/de/dailys/impedimento/ImpedimentoController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/impedimento")
@@ -23,5 +25,27 @@ public class ImpedimentoController {
     public ImpedimentoDTO cadastrarImpedimento(@RequestBody @Valid ImpedimentoDTO impedimento){
         Impedimento impedimentoACadastrar = modelMapper.map(impedimento, Impedimento.class);
         return modelMapper.map(impedimentoService.salvarImpedimento(impedimentoACadastrar),ImpedimentoDTO.class);
+    }
+
+    @GetMapping
+    public List<ImpedimentoDTO> exibirImpedimentos(){
+        List<ImpedimentoDTO> listaDeImpedimentos = new ArrayList<>();
+        for(Impedimento impedimentoReferencia: impedimentoService.exibirImpedimentos()){
+            ImpedimentoDTO impedimentoDTO = modelMapper.map(impedimentoReferencia, ImpedimentoDTO.class);
+            listaDeImpedimentos.add(impedimentoDTO);
+        }
+        return listaDeImpedimentos;
+    }
+
+    @PutMapping
+    public ImpedimentoDTO atualizarImpedimento(@RequestBody ImpedimentoDTO impedimentoDTO){
+        Impedimento impedimento = modelMapper.map(impedimentoDTO, Impedimento.class);
+        return modelMapper.map(impedimentoService.atualizarImpedimento(impedimento), ImpedimentoDTO.class);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarImpedimento(@PathVariable Long id){
+        impedimentoService.deletarImpedimento(id);
     }
 }
