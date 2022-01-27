01e1844d5ad4568faae708f747d1a1b3638d911e
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-27 20:10:56 -0300

    Implementar novo método de alterar dados na proximaTask

--- src/main/java/br/com/zup/gerenciador/de/dailys/proximaTask/ProximaTaskRepository.java
+++ src/main/java/br/com/zup/gerenciador/de/dailys/proximaTask/ProximaTaskRepository.java
@@ -3,6 +3,10 @@ package br.com.zup.gerenciador.de.dailys.proximaTask;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.lang.management.OperatingSystemMXBean;
+import java.util.Optional;
+
 @Repository
 public interface ProximaTaskRepository extends CrudRepository<ProximaTask, Long> {
+    Optional<ProximaTask> findById (Long id);
 }
