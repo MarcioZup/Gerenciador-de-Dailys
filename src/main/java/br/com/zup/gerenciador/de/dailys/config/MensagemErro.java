c8ce1869224ef47d89cc87218de90ab09f0e8a62
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-01-31 10:18:49 -0300

    implementar ControllerAdvisor

---
+++ src/main/java/br/com/zup/gerenciador/de/dailys/config/MensagemErro.java
@@ -0,0 +1,18 @@
+package br.com.zup.gerenciador.de.dailys.config;
+
+public class MensagemErro {
+    private String mensagemErro;
+
+    public MensagemErro(String mensagemErro) {
+        this.mensagemErro = mensagemErro;
+    }
+
+    public String getMensagemErro() {
+        return mensagemErro;
+    }
+
+    public void setMensagemErro(String mensagemErro) {
+        this.mensagemErro = mensagemErro;
+    }
+
+}
