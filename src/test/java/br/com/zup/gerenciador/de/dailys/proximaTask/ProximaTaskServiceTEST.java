f416b5ce8cdedc137a1a896e20d23f4e5d22aac6
Let Marçal <leticiamarcalmatias@gmail.com>
2022-02-02 13:45:44 -0300

    Implementar testes da service proximaTask

--- src/test/java/br/com/zup/gerenciador/de/dailys/proximaTask/ProximaTaskServiceTEST.java
+++ src/test/java/br/com/zup/gerenciador/de/dailys/proximaTask/ProximaTaskServiceTEST.java
@@ -1,9 +1,10 @@
 package br.com.zup.gerenciador.de.dailys.proximaTask;
 
+import br.com.zup.gerenciador.de.dailys.proximaTask.dtos.ProximaTaskSaidaDTO;
+import br.com.zup.gerenciador.de.dailys.proximaTask.exception.ProximaTaskInexistente;
 import br.com.zup.gerenciador.de.dailys.usuario.Usuario;
 import br.com.zup.gerenciador.de.dailys.usuario.UsuarioInexistente;
 import br.com.zup.gerenciador.de.dailys.usuario.UsuarioRepository;
-import com.fasterxml.jackson.databind.annotation.JsonTypeResolver;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import static org.mockito.Mockito.mock;
 
 @SpringBootTest
 public class ProximaTaskServiceTEST {
@@ -28,6 +31,8 @@ public class ProximaTaskServiceTEST {
     private ProximaTask proximaTask;
 
     private Usuario usuario;
+
+    private ProximaTaskSaidaDTO proximaTaskSaidaDTO;
 
     @BeforeEach
     private void setup(){
@@ -58,20 +63,66 @@ public class ProximaTaskServiceTEST {
 
     @Test
     public void testeSalvarProximaTaskPositivo(){
+        Mockito.when(usuarioRepository.save(Mockito.any(Usuario.class))).thenReturn(usuario);
         Mockito.when(usuarioRepository.existsById(Mockito.anyString())).thenReturn(true);
         Mockito.when(proximaTaskRepository.save(Mockito.any(ProximaTask.class))).thenReturn(proximaTask);
 
-        proximaTaskService.salvarProximaTask(usuario.getEmail());
+        proximaTaskRepository.save(proximaTask);
 
         Mockito.verify(proximaTaskRepository, Mockito.times(1)).save(proximaTask);
     }
 
     @Test
-    public void testeVisualizarProximasTasksCadastradas(){
+    public void testeVisualizarProximasTasksCadastradasPositivo(){
         List<ProximaTask> proximaTasks = Arrays.asList(proximaTask);
         Mockito.when(proximaTaskRepository.findAll()).thenReturn(proximaTasks);
         List<ProximaTask> proximaTasksResposta = (List<ProximaTask>) proximaTaskService.exibirProximasTasks();
         Mockito.verify(proximaTaskRepository, Mockito.times(1)).findAll();
+    }
+
+
+    @Test
+    public void testeAtualizarProximaTaskPositivo(){
+
+        Mockito.when(proximaTaskRepository.save(proximaTask)).thenReturn(proximaTask);
+        Mockito.when(proximaTaskRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(proximaTask));
+
+        final ProximaTaskSaidaDTO proximaTaskSaidaDTOMock = mock(ProximaTaskSaidaDTO.class);
+
+        proximaTaskService.atualizarProximaTask(Mockito.anyLong(), proximaTaskSaidaDTOMock);
+        Mockito.verify(proximaTaskRepository, Mockito.times(1)).save(proximaTask);
+    }
+
+    @Test
+    public void testeAtualizarProximaTaskNegativo(){
+        Mockito.when(proximaTaskRepository.save(Mockito.any())).thenReturn(proximaTask);
+        Mockito.when(proximaTaskRepository.findById(Mockito.anyLong())).thenReturn(Optional.empty());
+
+        final ProximaTaskSaidaDTO proximaTaskSaidaDTOMOCK = mock(ProximaTaskSaidaDTO.class);
+
+        ProximaTaskInexistente exception = Assertions.assertThrows(ProximaTaskInexistente.class,
+                () -> {proximaTaskService.atualizarProximaTask(Long.valueOf(0), proximaTaskSaidaDTOMOCK);});
+
+        Assertions.assertEquals("Não existe próxima task vinculado a este ID", exception.getMessage());
+    }
+
+    @Test
+    public void testeDeletarProximaTaskPositivo(){
+        Mockito.when(proximaTaskRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(proximaTask));
+        Mockito.doNothing().when(proximaTaskRepository).deleteById(Mockito.anyLong());
+        proximaTaskService.deletarProximaTask(proximaTask.getId());
+
+        Mockito.verify(proximaTaskRepository, Mockito.times(1)).deleteById(Mockito.anyLong());
+    }
+
+    @Test
+    public void testeDeletarProximaTaskNegativo(){
+        Mockito.doNothing().when(proximaTaskRepository).deleteById(Mockito.anyLong());
+
+        ProximaTaskInexistente exception = Assertions.assertThrows(ProximaTaskInexistente.class,
+                () -> {proximaTaskService.deletarProximaTask(Long.valueOf(0));});
+
+        Assertions.assertEquals("Não existe próxima task vinculado a este ID", exception.getMessage());
     }
 
 }
