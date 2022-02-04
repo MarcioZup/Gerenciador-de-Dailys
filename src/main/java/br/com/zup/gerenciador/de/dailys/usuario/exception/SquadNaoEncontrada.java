a6584e242c6df6538627afc95dee4aa5ca9ed29c
Let Marçal <leticiamarcalmatias@gmail.com>
2022-02-04 17:43:32 -0300

    Criar exception para Squad não encontrada

---
+++ src/main/java/br/com/zup/gerenciador/de/dailys/usuario/exception/SquadNaoEncontrada.java
@@ -0,0 +1,8 @@
+package br.com.zup.gerenciador.de.dailys.usuario.exception;
+
+public class SquadNaoEncontrada extends RuntimeException{
+
+    public SquadNaoEncontrada(String message) {
+        super(message);
+    }
+}
