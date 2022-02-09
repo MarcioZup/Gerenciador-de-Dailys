0e9cc2a08b713e22842bdbd92ecb8b0bb7b4d2b4
Let Marçal <leticiamarcalmatias@gmail.com>
2022-02-08 21:28:45 -0300

    Criar classe exception SquadNaoEncontrada

---
+++ src/main/java/br/com/zup/gerenciador/de/dailys/usuario/SquadNaoEncontrada.java
@@ -0,0 +1,7 @@
+package br.com.zup.gerenciador.de.dailys.usuario;
+
+public class SquadNaoEncontrada extends RuntimeException{
+    public SquadNaoEncontrada(String message) {
+        super(message);
+    }
+}
